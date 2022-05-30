74e0d225d78257cc65a0d9a017baa2e25499cfb1
jvnagarjuna <nagarjuna.jonnadula@gmail.com>
2022-05-30 14:48:55 +0530

    Chart Component Part 3

--- src/components/Analysis/Analysis.jsx
+++ src/components/Analysis/Analysis.jsx
@@ -1,16 +1,45 @@
 import React, { Component, Fragment } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis } from 'recharts';
 
 class Analysis extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        { Techonology: 'PHP', Projects: 100 },
+        { Techonology: 'MySqli', Projects: 90 },
+        { Techonology: 'Laravel', Projects: 95 },
+        { Techonology: 'React', Projects: 85 },
+        { Techonology: 'Opencart', Projects: 80 },
+        { Techonology: 'Vue Js', Projects: 70 },
+        { Techonology: 'Django', Projects: 60 },
+        { Techonology: 'JavaScript', Projects: 100 },
+      ],
+    };
+  }
+
   render() {
+    var blue = '#051b35';
     return (
       <Fragment>
         <Container className="text-center">
           <h1 className="serviceMainTitle">TECHNOLOGY USED</h1>
           <div className="bottom"></div>
           <Row>
-            <Col lg={6} md={12} sm={12}>
-              <h1>BarChart</h1>
+            <Col
+              style={{ width: '100%', height: '300px' }}
+              lg={6}
+              md={12}
+              sm={12}
+            >
+              <ResponsiveContainer>
+                <BarChart width={100} height={300} data={this.state.data}>
+                  <XAxis dataKey="Techonology" />
+                  <Tooltip />
+                  <Bar dataKey="Projects" fill={blue}></Bar>
+                </BarChart>
+              </ResponsiveContainer>
             </Col>
 
             <Col lg={6} md={12} sm={12}>
